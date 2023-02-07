801d0cd943ac81f6168ddc81cfa2aee476f8596f
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-02-07 14:50:16 +0100

    First time working on spring ! most important is being pragmatic !

---
+++ src/test/java/com/baha/TrainingPlatformEE/TrainingPlatformEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baha.TrainingPlatformEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlatformEeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
