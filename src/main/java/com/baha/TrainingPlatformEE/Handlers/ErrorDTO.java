559aeeff32a5bf06bbe623ca2a4d6358a2b03e30
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-02-15 23:00:44 +0100

    finishing the Global Exceptions handler

--- src/main/java/com/baha/TrainingPlatformEE/Handlers/ErrorDTO.java
+++ src/main/java/com/baha/TrainingPlatformEE/Handlers/ErrorDTO.java
@@ -1,12 +1,14 @@
 package com.baha.TrainingPlatformEE.Handlers;
 
 import com.baha.TrainingPlatformEE.Exceptions.ErrorCode;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
+@Builder
 @Data
 public class ErrorDTO {
     private Integer HttpCode ;
--- src/main/java/com/baha/TrainingPlatformEE/Handlers/RestExceptionHandler.java
+++ src/main/java/com/baha/TrainingPlatformEE/Handlers/RestExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.baha.TrainingPlatformEE.Handlers;
 
 import com.baha.TrainingPlatformEE.Exceptions.EntityNotFoundException;
+import com.baha.TrainingPlatformEE.Exceptions.InvalidEntityException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import com.baha.TrainingPlatformEE.Handlers.ErrorDTO ;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,6 +16,25 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleException (EntityNotFoundException exception , WebRequest webrequest)
     {
-
+        final HttpStatus NotFound = HttpStatus.NOT_FOUND ;
+         final ErrorDTO errorDTO =    ErrorDTO.builder()
+                    .errorcode(exception.getErrorCode())
+            .HttpCode(NotFound.value())
+            .Message(exception.getMessage())
+            .build() ;
+            //using error dto in the diamonds or not , won't affect !! spring already handles it !
+        return new ResponseEntity<ErrorDTO>(errorDTO,NotFound) ;
+    }
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<ErrorDTO> handleException (InvalidEntityException exception , WebRequest webRequest)
+    {
+       final HttpStatus BadRequest = HttpStatus.BAD_REQUEST ;
+        final ErrorDTO errorDTO = ErrorDTO.builder()
+                .errorcode(exception.getErrorCode())
+                .HttpCode(BadRequest.value())
+                .Message(exception.getMessage())
+                .errors(exception.getErrors())
+                .build() ;
+        return new ResponseEntity<>(errorDTO,BadRequest) ;
     }
 }
